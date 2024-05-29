4c1bca3f0c81763c50b90ea4b7727ab15e56ab76
Junaid Hassan <junaid.hassan@evampsaanga.com>
2024-05-29 20:03:07 +0500

    initial commit

--- lib/com_ens_eventus.dart
+++ lib/com_ens_eventus.dart
@@ -5,11 +5,13 @@ import 'dart:io';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_udid/flutter_udid.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class EventusUtil {
   static void postEvent() async {
     _getDeviceId();
     _getDeviceMake();
+    _getAppBuildNumber();
   }
 
   static Future<String> _getDeviceId() async {
@@ -31,5 +33,11 @@ class EventusUtil {
     } else {
       return "Unknown Platform Exception!";
     }
+  }
+
+  static Future<String> _getAppBuildNumber()async {
+     var packageInfo=await PackageInfo.fromPlatform();
+     debugPrint("eventusX:::AppBuildNumber:::${packageInfo.buildNumber}");
+     return packageInfo.buildNumber;
   }
 }
