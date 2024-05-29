fb52b1897a9c5703777ad9459ed4fbff1d93fc2d
Junaid Hassan <junaid.hassan@evampsaanga.com>
2024-05-29 19:51:46 +0500

    initial commit

--- lib/com_ens_eventus.dart
+++ lib/com_ens_eventus.dart
@@ -2,17 +2,34 @@ library com_ens_eventus;
 
 import 'dart:io';
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_udid/flutter_udid.dart';
 
 class EventusUtil {
- static void postEvent() async {
-    await _getDeviceId();
+  static void postEvent() async {
+    _getDeviceId();
+    _getDeviceMake();
   }
 
- static Future<String> _getDeviceId() async {
+  static Future<String> _getDeviceId() async {
     String udid = await FlutterUdid.udid;
     debugPrint("eventusX:::DeviceId:::$udid");
     return udid;
+  }
+
+  static Future<String> _getDeviceMake() async {
+    var deviceInfo = DeviceInfoPlugin();
+    if (Platform.isIOS) {
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      debugPrint("eventusX:::iOSBrand:::${iosDeviceInfo.name}");
+      return iosDeviceInfo.name;
+    } else if (Platform.isAndroid) {
+      var androidDeviceInfo = await deviceInfo.androidInfo;
+      debugPrint("eventusX:::AndroidBrand:::${androidDeviceInfo.brand}");
+      return androidDeviceInfo.brand; // unique ID on Android
+    } else {
+      return "Unknown Platform Exception!";
+    }
   }
 }
